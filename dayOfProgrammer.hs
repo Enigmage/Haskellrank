98346cbfc2a161dfb28c49bbafc0f3f2d55f8084
Enigmage <alizaidi0786.az@gmail.com>
2021-06-10 23:28:26 +0530

    day of programmer

--- dayOfProgrammer.hs
+++ dayOfProgrammer.hs
@@ -2,9 +2,12 @@
 
 getDate :: Int -> String
 getDate n 
-       | n < 1918 = if mod n 4 == 0 then "12.09." ++ show n else "13.09." ++ show n
+       | n < 1918 = if mod n 4 == 0 then leapCase else nonLeapCase
        | n == 1918 = "26.09." ++ show n
-       | otherwise = if mod n 400 == 0 || mod n 4 == 0 && mod n 100 /= 0 then "12.09." ++ show n else "13.09." ++ show n
+       | otherwise = if mod n 400 == 0 || mod n 4 == 0 && mod n 100 /= 0 then leapCase else nonLeapCase
+     where
+         leapCase = "12.09." ++ show n
+         nonLeapCase = "13.09." ++ show n
 
 
 main :: IO()
