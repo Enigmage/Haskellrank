b15766d1cddd42fd8a31f78e662c6291d8c54498
Enigmage <alizaidi0786.az@gmail.com>
2021-07-21 01:24:27 +0530

    new

--- save_prisoner.hs
+++ save_prisoner.hs
@@ -1,12 +1,12 @@
 -- https://www.hackerrank.com/challenges/save-the-prisoner/problem
 
-compute :: [Int] -> [Int]
-compute [] = []
-compute (n:m:s:xs) 
-  | index == 0 = n : compute xs -- since it is 1 based indexing.
-  | otherwise  = index : compute xs
+solve :: [Int] -> [Int]
+solve [] = []
+solve (n:m:s:xs) 
+  | index == 0 = n : solve xs -- since it is 1 based indexing.
+  | otherwise  = index : solve xs
   where
       index = mod ((s-1)+m) n
 
 main :: IO()
-main = interact $ unlines . map show . compute . map read . tail . words
+main = interact $ unlines . map show . solve . map read . tail . words
